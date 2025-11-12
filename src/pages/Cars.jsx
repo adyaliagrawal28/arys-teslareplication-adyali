4b9b5415c7fb54d775de3040c262383cf785fdf6
Adyali Agrawal <adyaliagrawal28@gmail.com>
2025-11-12 20:12:30 +0530

    Setup project structure and basic routing

---
+++ src/pages/Cars.jsx
@@ -0,0 +1,7 @@
+export default function Cars() {
+  return (
+    <div className="h-screen flex items-center justify-center text-4xl font-bold">
+      Cars Page
+    </div>
+  )
+}
---
+++ src/pages/Customize.jsx
@@ -0,0 +1,7 @@
+export default function Customize() {
+  return (
+    <div className="h-screen flex items-center justify-center text-4xl font-bold">
+      Customize Page
+    </div>
+  )
+}
